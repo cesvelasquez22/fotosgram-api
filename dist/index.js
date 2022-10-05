077a58cd438ad64c76aab1adc35c762b93688303
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-04 22:46:51 -0600

    + connect mongodb

--- dist/index.js
+++ dist/index.js
@@ -5,8 +5,15 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const server_1 = __importDefault(require("./classes/server"));
 const user_routes_1 = __importDefault(require("./routes/user.routes"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const server = new server_1.default();
-server.app.use('/user', user_routes_1.default);
+server.app.use("/user", user_routes_1.default);
+// Connect to database
+mongoose_1.default.connect("mongodb://localhost:27017/fotosgram", {}, (err) => {
+    if (err)
+        throw err;
+    console.log("Database online");
+});
 server.start(() => {
     console.log(`Servidor corriendo en puerto ${server.port}`);
 });
--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import Server from "./classes/server";
 import userRoutes from "./routes/user.routes";
+import mongoose from "mongoose";
 
 const server = new Server();
 
-server.app.use('/user', userRoutes);
+server.app.use("/user", userRoutes);
+
+// Connect to database
+mongoose.connect("mongodb://localhost:27017/fotosgram", {}, (err) => {
+  if (err) throw err;
+  console.log("Database online");
+});
 
 server.start(() => {
-    console.log(`Servidor corriendo en puerto ${ server.port }`);
+  console.log(`Servidor corriendo en puerto ${server.port}`);
 });
