7dcdd8763677382957db22774b83fedb946b19d5
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-10-06 19:55:14 -0600

    + Get paginated posts

--- dist/routes/post.routes.js
+++ dist/routes/post.routes.js
@@ -13,12 +13,23 @@ const express_1 = require("express");
 const auth_1 = require("../middlewares/auth");
 const post_model_1 = require("../models/post.model");
 const postRoutes = (0, express_1.Router)();
-postRoutes.get("/", (req, res) => {
+postRoutes.get("/", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const page = Number(req.query.page) || 1;
+    let skip = page - 1;
+    skip = skip * 10;
+    const posts = yield post_model_1.Post.find()
+        .sort({ _id: -1 })
+        .skip(skip)
+        .limit(10)
+        .populate("user", "-password")
+        .exec();
     res.json({
         ok: true,
-        message: "Everything is ok",
+        page,
+        results: posts.length,
+        posts,
     });
-});
+}));
 postRoutes.post("/", [auth_1.verifyToken], (req, res) => {
     const body = req.body;
     body.user = req.user._id;
--- routes/post.routes.ts
+++ routes/post.routes.ts
@@ -4,10 +4,23 @@ import { Post } from "../models/post.model";
 
 const postRoutes = Router();
 
-postRoutes.get("/", (req, res) => {
+postRoutes.get("/", async (req, res) => {
+  const page = Number(req.query.page) || 1;
+  let skip = page - 1;
+  skip = skip * 10;
+
+  const posts = await Post.find()
+    .sort({ _id: -1 })
+    .skip(skip)
+    .limit(10)
+    .populate("user", "-password")
+    .exec();
+
   res.json({
     ok: true,
-    message: "Everything is ok",
+    page,
+    results: posts.length,
+    posts,
   });
 });
 
